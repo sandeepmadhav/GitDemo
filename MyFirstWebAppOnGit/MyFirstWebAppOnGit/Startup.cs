399db476a9d931ac61ba54177e200bbd5add6684
sandeepmadhavasetty <sandeep.madhavasetty7@gmail.com>
2021-10-22 17:47:53 +0530

    commit second one

--- MyFirstWebAppOnGit/MyFirstWebAppOnGit/Startup.cs
+++ MyFirstWebAppOnGit/MyFirstWebAppOnGit/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+//test
 namespace MyFirstWebAppOnGit
 {
     public class Startup
