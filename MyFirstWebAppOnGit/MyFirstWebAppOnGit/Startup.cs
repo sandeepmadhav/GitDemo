88852b9d7495743584fc81e86523fb539f58419c
sandeepmadhavasetty <sandeep.madhavasetty7@gmail.com>
2021-10-22 17:50:32 +0530

    fsafdsa

--- MyFirstWebAppOnGit/MyFirstWebAppOnGit/Startup.cs
+++ MyFirstWebAppOnGit/MyFirstWebAppOnGit/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-//test fdsafd
+//test fdsafdfsddf
 namespace MyFirstWebAppOnGit
 {
     public class Startup
